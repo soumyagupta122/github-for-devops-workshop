477ad0f2546a269fb4411b65dc4750ddf55cc7e4
Soumya Gupta <122059168+soumyagupta122@users.noreply.github.com>
2025-06-22 23:29:29 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,1 +1,14 @@
-print("dont know what im doing but it is what it is..")
+"""
+This module contains a simple hello function.
+"""
+
+
+def hello():
+    """
+    This function returns a greeting message.
+    """
+    return "Hello Dosto"
+
+
+if __name__ == "__main__":
+    hello()
